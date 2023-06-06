024d4786e3e585042c0fd6f9cfd5cfb1c5ac6fe3
cjsolis <cjelizondo12@gmail.com>
2023-06-06 15:03:53 -0500

    added node modules and config file for express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const db = require('./config/connection')
+
+const app = express();
+const port = 3001;
+
+const dbName = 'socialNetworkDB'
+
+app.listen(port, () =>{
+    console.log(`API server running on ${port}`)
+})
