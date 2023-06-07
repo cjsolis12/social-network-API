613d6cb28583ec78ea92c138e59acf81145a15b8
cjsolis <cjelizondo12@gmail.com>
2023-06-07 14:54:43 -0500

    created get, post, update, and delete routes for user and tested

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
 
+
+
+//GET all users
 router.get('/',(req,res)=>{
     res.json({ message: "great work"})
+})
+
+//GET one user
+router.get('/',(req,res)=>{
+    res.json({ message: "great work"})
+})
+
+//Create a user
+router.post('/',(req,res)=>{
+    res.json({ message: "great work"})
+})
+
+//Update a User
+router.put('/:id',(req,res)=>{
+    res.json({ message: `update user ${req.params.id}`})
+})
+
+//Delete a user
+router.delete('/:id',(req,res)=>{
+    res.json({ message: `delete user ${req.params.id}`})
 })
 
 module.exports= router
