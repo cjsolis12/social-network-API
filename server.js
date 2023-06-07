e93a1c304214d73350dba765a138d7dccd7030e7
cjsolis <cjelizondo12@gmail.com>
2023-06-07 14:45:05 -0500

    added api folder in routes with userRoutes and tested GET

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv').config()
 const db = require('./config/connection')
 const routes = require ('./routes')
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
 
 // const dbName = 'socialNetworkDB'
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(routes);
+
+
 
 db.once('open', () => {
     app.listen(port, () =>{
