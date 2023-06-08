64dae8ca8869b85e7df90de38adb9d56cc8c29c9
cjsolis <cjelizondo12@gmail.com>
2023-06-07 19:00:43 -0500

    added thought, user, and reaction schema

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
-const { connect, connection } = require('mongoose');
+const { connect, connection } = require("mongoose");
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialNetworkDB";
 
 connect(connectionString);
 
 module.exports = connection;
-
--- models/thought.js
+++ models/thought.js
@@ -1,15 +1,61 @@
-const { Schema, model, mongo, default: mongoose } = require('mongoose');
+const { Schema, model, mongo, default: mongoose } = require("mongoose");
 
+//Reaction Schema
+const reactionSchema = new Schema({
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: function (timestamp) {
+        return new Date(timestamp).toLocaleString();
+      },
+    },
+  });
 //Schema to create thought model
-
 const thoughtSchema = new Schema(
-{
+  {
     thoughtText: {
-        type: String,
-        required: true,
-        minlength:[1, "Thought text must be at least 1 character long"],
-        maxlength: [280, "Thought text"]
-    }
-}
+      type: String,
+      required: true,
+      minlength: [1, "Thought text must be at least 1 character long"],
+      maxlength: [280, "Thought text cannot be longer than 280 characters"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: function (timestamp) {
+        return new Date(timestamp).toLocaleString();
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: { virtuals: true },
+    id: false,
+  }
+);
 
-)
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+
+
+
